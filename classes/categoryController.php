d49bf30a224de39c6a922680f8e442d31cd9cc11
Alexandra Skopina <44308291+skopina-alexandra@users.noreply.github.com>
2022-09-18 17:56:14 +1000

    Update categoryController.php

--- classes/categoryController.php
+++ classes/categoryController.php
@@ -3,9 +3,6 @@
 class CategoryController extends Category{
     public function importCategoriesFromFile($filename){
         $categoriesData = json_decode(file_get_contents($filename), true);
-        // echo "<pre>";
-        // var_dump($categoriesData);
-        // echo "</pre>";
         foreach ($categoriesData as $category) {
             $this->addCategory($category);
         }
